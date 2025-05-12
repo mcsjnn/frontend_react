4d264313efaad7910622ba8cf9e9615c6065001d
mcsjnn <sujenn.ancco@tecsup.edu.pe>
2025-05-11 23:31:30 -0500

    Actualizacion de user.services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/test/";
+const API_URL = process.env.REACT_APP_API_URL + "/test/";
 
 const getPublicContent = async () => {
   try {
