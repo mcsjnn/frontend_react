43d895d16b044008ec6bc717da846d5e9d2b6b55
mcsjnn <sujenn.ancco@tecsup.edu.pe>
2025-05-12 00:35:34 -0500

    Fix: redireccion de axios

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth/";
+const API_URL = import.meta.env.REACT_VITE_API_URL;
 
 const register = (data) => axios.post(API_URL + "signup", data);
 
